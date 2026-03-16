70a36958c11a22cce7dc5af0347a4ff63473264f
Athan1930 <jnthncayanan@gmail.com>
2026-03-16 17:46:45 +0800

    Update data.php

--- data.php
+++ data.php
@@ -1,7 +1,9 @@
 <?php
 header("Content-Type: application/json");
 header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Methods: GET");
+header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Allow-Methods: OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, X-API-Key");
 
 // Handle preflight OPTIONS request (for CORS)
